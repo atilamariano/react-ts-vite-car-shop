fd26f10a513b2b509157e4a3b6ca44d745d32253
Atila Mariano <103537702+atilamariano@users.noreply.github.com>
2023-04-10 15:45:36 -0300

    feat: store state inside localStorage

--- src/context/AuthProvider/index.tsx
+++ src/context/AuthProvider/index.tsx
@@ -1,12 +1,20 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { IAuthProvider, IContext, IUser } from "./types";
 import Password from "antd/es/input/Password";
-import { LoginRequest } from "./util";
+import { LoginRequest, getUserLocalStorage, setUserLocalStorage } from "./util";
 
 export const AuthContext = createContext<IContext>({} as IContext)
 
 export const AutheProvider = ({ children }: IAuthProvider) => {
-    const [user, setUser] = useState<IUser | null>()
+    const [user, setUser] = useState<IUser | null>();
+
+    useEffect(() => {
+       const user = getUserLocalStorage();
+       
+       if (user) {
+        setUser(user);
+       }
+    }, [])
 
     async function authenticate(email: string, password: string) {
         const response = await LoginRequest(email, password)
@@ -14,10 +22,12 @@ export const AutheProvider = ({ children }: IAuthProvider) => {
         const payload = { token: response.token, email };
 
         setUser(payload);
+        setUserLocalStorage(payload);
     }
 
     function logout() {
         setUser(null);
+        setUserLocalStorage(null);
     }
 
     return (
